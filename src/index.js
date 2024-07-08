f8509c53eedaf0bc97a5f3256332de5a3dc78cef
zogpap <zogpap@163.com>
2024-07-08 19:01:06 +0800

    feat:新增ModalButton、ModalTitle平台判断(text默认样式)

--- src/index.js
+++ src/index.js
@@ -14,14 +14,21 @@ import Modal from './Modal';
 import BottomModal from './BottomModal';
 import DraggableView from './components/DraggableView';
 import Backdrop from './components/Backdrop';
-import ModalTitle from './components/ModalTitle';
+import ModalTitleOther from './components/ModalTitle';
+import ModalTitleHarmony from './components/ModalTitle.harmony';
 import ModalFooter from './components/ModalFooter';
-import ModalButton from './components/ModalButton';
+import ModalButtonOther from './components/ModalButton';
+import ModalButtonHarmony from './components/ModalButton.harmony';
 import ModalContent from './components/ModalContent';
 import Animation from './animations/Animation';
 import FadeAnimation from './animations/FadeAnimation';
 import ScaleAnimation from './animations/ScaleAnimation';
 import SlideAnimation from './animations/SlideAnimation';
+import {Platform} from 'react-native'
+const isHarmony = Platform.OS === 'harmony';
+
+const ModalTitle = (isHarmony ? ModalTitleHarmony : ModalTitleOther);
+const ModalButton = (isHarmony ? ModalButtonHarmony : ModalButtonOther);
 
 export {
   ModalPortal,
